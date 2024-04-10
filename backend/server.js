f6f1746939eea32d278fb89524c2f19793de5180
currencytechnologies <currencytechnologies@gmail.com>
2024-04-10 14:29:34 +0000

    commit all

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
-
-// Use port and dbUrl as needed in your application
-app.listen(port, () => {
-  console.log('Server running on port 3000');
-});
